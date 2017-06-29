e12cc6a3b01fd665571003a648299784e9b75b6a
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-06-29 13:15:24 +0200

    .

---
+++ index.js
@@ -0,0 +1,15 @@
+var reg = new RegExp('([^?=&]+)(=([^&]*))?', 'g')
+var assert = require('assert')
+
+module.exports = qs
+
+function qs (uri) {
+  assert.ok(typeof window !== 'undefined', 'nanoquery: expected window to exist')
+
+  var obj = {}
+  uri.replace(/^.*\?/, '').replace(reg, function (a0, a1, a2, a3) {
+    obj[window.decodeURIComponent(a1)] = window.decodeURIComponent(a3)
+  })
+
+  return obj
+}
