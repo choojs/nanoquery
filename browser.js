fe6509ed3594de295573f300ad9576297ee3c4ec
Boris Serdiuk <just-boris@hotmail.com>
2017-11-14 14:48:33 +0100

    remove reference of window in the code

--- browser.js
+++ browser.js
@@ -5,11 +5,10 @@ module.exports = qs
 
 function qs (url) {
   assert.equal(typeof url, 'string', 'nanoquery: url should be type string')
-  assert.ok(typeof window !== 'undefined', 'nanoquery: expected window to exist')
 
   var obj = {}
   url.replace(/^.*\?/, '').replace(reg, function (a0, a1, a2, a3) {
-    obj[window.decodeURIComponent(a1)] = window.decodeURIComponent(a3)
+    obj[decodeURIComponent(a1)] = decodeURIComponent(a3)
   })
 
   return obj
